65b105113c323d09e0e56893977b39115204e19e
João Pedro <joao2015.76@gmail.com>
2024-03-16 11:55:32 -0300

    Models for orders

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 app = express();
 
@@ -44,8 +46,13 @@ Cart.belongsTo(User);
 User.hasOne(Cart);
 Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart, {through: CartItem});
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem});
 
-sequelize.sync()
+
+
+sequelize.sync( {force: true })
     .then(result => {
         return User.findByPk(1);
     })
